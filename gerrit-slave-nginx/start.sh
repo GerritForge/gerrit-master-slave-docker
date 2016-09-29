e096ee7bb500f1f73170123da0319c2311e6c0b6
Luca Milanesio <luca.milanesio@gmail.com>
2016-09-29 22:44:42 +0100

    Compress and tidy-up slaves
    Reduce the number of Dockerfile steps to consume less
    disk space and make the image creation much quicker.
    Additionally master and slaves have more meaningful names.
    
    Introduce both NGINX and Apache2 based slaves
    
    Change-Id: I34fec4a52bb49f40c89fba617e3f381c2a28aaf1

---
+++ gerrit-slave-nginx/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+chown -R gerrit: /var/gerrit/.ssh
+chmod 600 /var/gerrit/.ssh/authorized_keys
+
+/etc/init.d/ssh start
+/etc/init.d/nginx start
+/etc/init.d/fcgiwrap start
+
+wait-for-it.sh -t 60 gerrit-master:8080 -- echo "Gerrit master is up"
+/etc/init.d/gerrit start &
+touch  /var/gerrit/logs/error_log && chown -R gerrit: /var/gerrit && tail -f /var/gerrit/logs/error_log
